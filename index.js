3277122e561b6e6a5ff21de59c66d6104b81afbb
Lydia Bell <lrbell84@gmail.com>
2021-07-10 23:22:16 +0000

    commit tree dd3e9798168300330edf30d648023d39893fa956

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(duo) {
+    return duo.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(duo) {
+    return duo.slice(-2);
+};// Code your solution in this file!
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiply) {
+    return function (worth) {
+        return fareMultiply * worth;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (duo, returnDrivers) {
+    return returnDrivers (duo);
+};
+
+
